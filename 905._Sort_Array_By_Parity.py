956b7be766f63d74a07cb7b3e52eeb09e1dbbc45
Mohamed Kaddouri <yeeloman@TheJester.eldergods.org>
2023-09-28 02:56:32 +0100

    905. Sort Array By Parity

---
+++ 905._Sort_Array_By_Parity.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        new_list = []
+        for i, num in enumerate(nums):
+            if nums[i] % 2 == 0:
+                new_list = [nums[i]] + new_list
+            elif nums[i] % 2 == 1:
+                new_list = new_list + [nums[i]]
+        return new_list
