70944db10b5850b1a87600caf88b85a91cc451b7
Mohamed Kaddouri <yeeloman@TheJester.eldergods.org>
2023-10-01 17:42:25 +0100

    13. Roman to Integer

--- 13._Roman_to_Integer.py
+++ 13._Roman_to_Integer.py
@@ -12,3 +12,15 @@ class Solution:
                 result += val
             pre_val = val
         return result
+
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+#         result = 0
+#         rom_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M":1000}
+#         for i in range(len(s) - 1):
+#             if rom_num[s[i]] < rom_num[s[i + 1]]:
+#                 result -= rom_num[s[i]]
+#             else:
+#                 result += rom_num[s[i]]
+#         result += rom_num[s[-1]]
+#         return result
